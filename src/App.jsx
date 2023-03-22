17bacad66140fbc4043bfe0d0fb482822b6d1d7f
Doragon <moulaykadii2001@gmail.com>
2023-03-22 23:12:32 +0000

    initiate project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './styles'
+import Navbar from './Navbar'
+import Stats from './Stats'
 function App() {
   return (
     <div className='bg-primary w-full overflow-hidden'>
@@ -11,6 +13,18 @@ function App() {
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           Hero
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
         </div>
       </div>
     </div>
