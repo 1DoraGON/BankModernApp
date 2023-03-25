46ba47ad1526bac8856f43ba8e2bf58a1f83b447
Doragon <moulaykadii2001@gmail.com>
2023-03-25 19:05:26 +0000

    playing around with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,45 @@ import React, { useState } from 'react'
 import { close,logo,menu } from '../assets'
 import { navLinks } from '../constants/constants'
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className="w-full flex py-6 justyfy-between
     items-center navbar">
       <img src={logo} alt="bank" className='w-[124px] h-[32px]'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-        <li className='text-white'>hello</li>
-        <li className='text-white'>hello</li>
-        <li className='text-white'>hello</li>
-        <li className='text-white'>hello</li>
+        {navLinks.map((navLink,index) => (
+          <li key={navLink.id} className={`font-poppins font-normal
+            text-[16px] text-white 
+            ${index===navLinks.length -1? 'mr-0' : 'mr-10'}`}>
+            <a href={`#${navLink.id}`}>
+              {navLink.title}
+            </a>
+          </li>
+        ))}
+
       </ul>
+      <div className="sm:hidden flex flex-1 
+        justify-end items-center">
+          <img src={toggle ? close : menu } alt="menu"
+          className='w-[20px] h-[28px] object-contain' 
+          onClick={()=>{
+            setToggle((prev)=>!prev)
+          }}/>
+          <div className={`${toggle? 'flex' : 'hidden'} p-6 bg-black-gradient
+            absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+              <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+                {navLinks.map((navLink,index) => (
+                  <li key={navLink.id} className={`font-poppins font-normal
+                    text-[16px] text-white 
+                    ${index===navLinks.length -1? 'mr-0' : 'mr-10'}`}>
+                    <a href={`#${navLink.id}`}>
+                      {navLink.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+      </div>
     </nav>
   )
 }
